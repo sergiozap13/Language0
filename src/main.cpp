744595c34504702984edcf3b1e4424a5a3ab0120
Sergio Zapata <56563008+sergiozap13@users.noreply.github.com>
2023-03-05 14:19:18 +0100

    Main Ready

--- src/main.cpp
+++ src/main.cpp
@@ -31,19 +31,23 @@ using namespace std;
 
 //Includes de nuestras clases
 
-#include "Bigram.h";
+#include "Bigram.h"
+
 
 int main(int argc, char* argv[]) {
     // This string contains the list of characters that are considered as
     // valid within a word. The rest of characters are considered as
     // separators
-    const std::string validCharacters = "s";
+    const std::string validCharacters = "aeemeebcdefghijklmnopqrstuvwxyz\xE0\xE1\xE2\xE3\xE4\xE5\xE6\xE7\xE8\xE9\xEA\xEB\xEC\xED\xEE\xEF\xF0\xF1\xF2\xF3\xF4\xF5\xF6\xF8\xF9\xFA\xFB\xFC\xFD\xFE\xFF";
     cout << validCharacters << endl;
+    
+    
+    // Probando método: 
     char a = 'b';
     char b = 'c';
-    bool resultado = isValidCharacter(a , validCharacters);
     
-    cout << resultado;
+    bool resultado = isValidCharacter(a, validCharacters);
+    
     // Read a text
     // Read a bigram (tex with two characters)
     
