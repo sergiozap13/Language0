43ba9e8458327d40894db5adb3c4d2e45c777b13
Sergio Zapata <56563008+sergiozap13@users.noreply.github.com>
2023-03-05 13:57:54 +0100

    Creación Bigram.cpp

---
+++ src/Bigram.cpp
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Bigram.cpp
+ * Author: sergio
+ * 
+ * Created on 5 de marzo de 2023, 13:44
+ */
+
+#include "Bigram.h"
+
+Bigram::Bigram(const std::string& text) {
+}
+
+Bigram::Bigram(char first, char second) {
+}
+
+std::string Bigram::getText() const{
+}
+
+std::string Bigram::toString() const{
+    
+}
+
+const char& Bigram::at(int index) const{
+    
+}
+
+char& Bigram::at(int index){
+    
+}
+
+bool isValidCharacter(char character, const std::string& validCharacters){
+    /*
+    for(int i = 0; i < validCharacters.length(); i++){
+        if (character == validCharacters[i]){
+            return true;
+        }
+    }
+    
+    return false;
+     * 
+     * */
+}
+
+void toUpper(Bigram &bigram){
+    
+}
